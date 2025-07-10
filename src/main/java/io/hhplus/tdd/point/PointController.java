cd73a0e2c5a87ff9efc294d0bc2c6d31eda08685
donghyeon.lim <ehdgus0221@naonsoft.com>
2025-07-10 11:38:12 +0900

    refactor : 컨트롤러 주석 수정

--- src/main/java/io/hhplus/tdd/point/PointController.java
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -18,7 +18,7 @@ public class PointController {
 
 
     /**
-     * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
+     * 특정 유저의 포인트를 조회
      */
     @GetMapping("{id}")
     public UserPoint point(
@@ -28,7 +28,7 @@ public class PointController {
     }
 
     /**
-     * TODO - 특정 유저의 포인트 충전/이용 내역을 조회하는 기능을 작성해주세요.
+     * 특정 유저의 포인트 충전/이용 내역을 조회
      */
     @GetMapping("{id}/histories")
     public List<PointHistory> history(
@@ -36,6 +36,10 @@ public class PointController {
     ) {
         return pointService.getHistories(id);
     }
+
+    /**
+     * 특정 유저의 포인트를 충전
+     */
 
     @PatchMapping("{id}/charge")
     public UserPoint charge(
@@ -46,7 +50,7 @@ public class PointController {
     }
 
     /**
-     * TODO - 특정 유저의 포인트를 사용하는 기능을 작성해주세요.
+     * 특정 유저의 포인트를 사용
      */
     @PatchMapping("{id}/use")
     public UserPoint use(
